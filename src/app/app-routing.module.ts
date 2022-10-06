e2aa72b68d7b2c4f93b92f3a8cedc08c3cbf7899
RBBeny <benjaramirez2501@gmail.com>
2022-10-05 19:36:14 -0500

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,23 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'homeLog',
     redirectTo: 'folder/Inbox',
     pathMatch: 'full'
   },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
+
 ];
 
 @NgModule({
